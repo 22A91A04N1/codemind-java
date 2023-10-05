54cc9256c30e0787d61ec5d0f730f514c884afdb
22A91A04N1 <119719186+22A91A04N1@users.noreply.github.com>
2023-10-05 19:47:52 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String n;         n=sc.nextLine();         System.out.print(n);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String n;
+        n=sc.nextLine();
+        System.out.print(n);
+    }
+}
