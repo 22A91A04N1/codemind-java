c7dba52e6a0a04413a1404e0eaf35a44b6499bc7
22A91A04N1 <119719186+22A91A04N1@users.noreply.github.com>
2023-10-05 19:25:54 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a;         n=sc.nextInt();         a=sc.nextInt();         System.out.println(n/a);     } }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a;
+        n=sc.nextInt();
+        a=sc.nextInt();
+        System.out.println(n/a);
+    }
+}
